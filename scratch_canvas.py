b7fee6320f890876aa64922deca56402e61f7937
Lakshita1928 <95125694+Lakshita1928@users.noreply.github.com>
2022-03-05 16:51:26 +0530

    Add files via upload

---
+++ scratch_canvas.py
@@ -0,0 +1,8 @@
+import turtle
+marks = int(input('Enter your marks'))
+if 0 >= 80:
+    print('Excellent work!')
+elif 0 >= 60:
+    print('Good work!')
+elif 0 <= 60:
+    print('You can do better')
